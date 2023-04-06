8e972091841b37d28b8aca407e3df04586f859f2
tiago80673 <trrncx@gmail.com>
2023-04-06 23:54:29 +0100

    processamento de meta com apenas um ponto

--- Code/getLappedData.m
+++ Code/getLappedData.m
@@ -1,6 +1,6 @@
-function data = getAllData(filename, meta)
-    [battery,gps,susp,controller,temp] = dataProces(filename);
+function data = getLappedData(battery,gps,susp,controller,temp,meta)
     lapTime = lapSeparate (gps,meta);
+    disp(lapTime);
     data = lapOrganize(lapTime,battery,controller,gps,susp,temp);
-
 end
+
